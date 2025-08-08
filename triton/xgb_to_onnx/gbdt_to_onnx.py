7eebed7c13d694d761d138586698df568b8e5359
bee-eater <minim4@bee-eater-mhyMac-mini.local>
2025-08-08 20:28:21 +0800

    add xgb model.bin

--- triton/xgb_to_onnx/gbdt_to_onnx.py
+++ triton/xgb_to_onnx/gbdt_to_onnx.py
@@ -13,64 +13,20 @@ def gbdt_to_onnx(gbdt_path, onnx_path, model_type="auto", test_run=True):
     booster.load_model(gbdt_path)
 
     booster.save_model("model.json")
-    booster.save_model("model.onnx")
+    booster.save_model("model.bin")
 
-    # with open("model.onnx", "wb") as f:
-    #     f.write(onnx_model)
 
-    # 2. 自动检测特征数（通过 dump_model 的 JSON）
-    booster_json = json.loads(booster.save_raw("json").decode("utf-8"))
-    # print(json.dumps(booster_json, indent=2, ensure_ascii=False))
-    # first_tree = booster_json[0]
-    # feature_ids = set()
-    #
-    # def collect_features(node):
-    #     if "split" in node:
-    #         feature_ids.add(node["split"])
-    #         collect_features(node["children"][0])
-    #         collect_features(node["children"][1])
-    #
-    # collect_features(first_tree)
     n_features = 514
 
     print(f"检测到特征数: {n_features}")
 
-    # 3. 自动选择分类或回归
-    # if model_type == "auto":
-    #     if booster.attr("objective") and "reg" in booster.attr("objective"):
-    #         model_type = "regressor"
-    #     else:
-    #         model_type = "classifier"
-    #
-    # model_type = "regressor"
-    #
-    # print(f"模型类型: {model_type}")
-    #
-    # # 4. 转成 sklearn API 模型
-    # if model_type == "classifier":
-    #     model = xgb.XGBClassifier()
-    # else:
-    #     model = xgb.XGBRegressor()
-    # model._Booster = booster
-    #
-    # # 5. 定义输入
-    # initial_type = [('input', FloatTensorType([None, n_features]))]
-    #
-    # # 6. 转换成 ONNX
-    # onnx_model = convert_sklearn(model, initial_types=initial_type)
-    #
-    # # 7. 保存
-    # with open(onnx_path, "wb") as f:
-    #     f.write(onnx_model.SerializeToString())
-    #
-    # print(f"已保存 ONNX 模型到: {onnx_path}")
 
     # 8. 可选：测试推理
-    # if test_run:
-    #     session = ort.InferenceSession(onnx_path)
-    #     inputs = np.random.rand(1, n_features).astype(np.float32)
-    #     output = session.run(None, {'input': inputs})
-    #     print("推理输出:", output)
+    if test_run:
+        session = ort.InferenceSession(onnx_path)
+        inputs = np.random.rand(1, n_features).astype(np.float32)
+        output = session.run(None, {'input': inputs})
+        print("推理输出:", output)
 
 
 if __name__ == "__main__":
